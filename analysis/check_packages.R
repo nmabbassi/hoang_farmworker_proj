330257c2949475af56c356c306e0935754e4e1bf
Nakiessa Abbassi <nabbassi@uoregon.edu>
2020-06-30 13:19:31 -0700

    add packages

--- analysis/check_packages.R
+++ analysis/check_packages.R
@@ -5,7 +5,7 @@
 #http://www.vikram-baliga.com/blog/2015/7/19/a-hassle-free-way-to-verify-that-r-packages-are-installed-and-loaded
 
 #add new packages to the chain here
-packages = c("foreign","ggplot2","stargazer")
+packages = c("foreign","ggplot2","stargazer","readxl","tidyverse", "dplyr", "ggrepel")
 
 package.check <- lapply(packages, FUN = function(x) {
   if (!require(x, character.only = TRUE)) {
